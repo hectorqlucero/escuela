6cf5739aa68bcca0c37ef178d050be7596e62e52
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2019-07-19 19:40:36 -0700

    Added the alumnos table creation logic

--- src/sk/models/cdb.clj
+++ src/sk/models/cdb.clj
@@ -36,16 +36,37 @@
     :level     "S"
     :active    "T"}])
 
+(def alumnos-sql
+  "CREATE TABLE alumnos (
+   matricula int(11) unsigned NOT NULL,
+   password varchar(50) DEFAULT NULL,
+   apell_paterno varchar(100) DEFAULT NULL,
+   apell_materno varchar(100) DEFAULT NULL,
+   nombre varchar(100) DEFAULT NULL,
+   escuela varchar(100) DEFAULT NULL,
+   carrera varchar(100) DEFAULT NULL,
+   semestre int(11) DEFAULT NULL,
+   status char(1) DEFAULT NULL COMMENT 'E=estudiante, G=Egresado',
+   fecha_ingreso date DEFAULT NULL,
+   fecha_egreso date DEFAULT NULL,
+   email varchar(100) DEFAULT NULL,
+   foto varchar(100) DEFAULT NULL,
+   PRIMARY KEY (matricula)
+   ) ENGINE=InnoDB DEFAULT CHARSET=utf8")
+
 (defn create-database []
   "Creates database and a default admin users"
   (Query! db users-sql)
-  (Insert-multi db :users user-rows))
+  (Insert-multi db :users user-rows)
+  (Query! db alumnos-sql))
 
 (defn reset-database []
   "Removes existing tables and re-creates them"
   (Query! db "DROP table IF EXISTS users")
   (Query! db users-sql)
-  (Insert-multi db :users user-rows))
+  (Insert-multi db :users user-rows)
+  (Query! db "DROP table IF EXISTS alumnos")
+  (Query! db alumnos-sql))
 
 (defn migrate []
   "Migrate by the seat of my pants"
