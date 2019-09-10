834476c71d4811fffc135c39403a6b3f2443b51e
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2019-09-10 10:30:45 -0700

    Added foreign key constraints

--- src/sk/models/cdb.clj
+++ src/sk/models/cdb.clj
@@ -5,7 +5,7 @@
 ;; Start users table
 (def users-sql
   "CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   lastname varchar(45) DEFAULT NULL,
   firstname varchar(45) DEFAULT NULL,
   username varchar(45) DEFAULT NULL,
@@ -15,8 +15,7 @@
   phone varchar(45) DEFAULT NULL,fax varchar(45) DEFAULT NULL,
   email varchar(100) DEFAULT NULL,
   level char(1) DEFAULT NULL COMMENT 'A=Administrador,U=Usuario,S=Sistema',
-  active char(1) DEFAULT NULL COMMENT 'T=Active,F=Not active',
-  PRIMARY KEY (id)
+  active char(1) DEFAULT NULL COMMENT 'T=Active,F=Not active'
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8")
 
 (def user-rows
@@ -41,7 +40,7 @@
 ;; Start alumnos table
 (def alumnos-sql
   "CREATE TABLE alumnos (
-   matricula int(11) unsigned NOT NULL,
+   matricula int(11) unsigned NOT NULL PRIMARY KEY,
    password varchar(50) DEFAULT NULL,
    apell_paterno varchar(100) DEFAULT NULL,
    apell_materno varchar(100) DEFAULT NULL,
@@ -53,17 +52,15 @@
    fecha_ingreso date DEFAULT NULL,
    fecha_egreso date DEFAULT NULL,
    email varchar(100) DEFAULT NULL,
-   foto varchar(100) DEFAULT NULL,
-   PRIMARY KEY (matricula)
+   foto varchar(100) DEFAULT NULL
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8")
 ;; End alumnos table
 
 ;; Start categorias table
 (def categorias-sql
   "CREATE TABLE categorias (
-   id int(11) unsigned NOT NULL AUTO_INCREMENT,
-   categoria varchar(100) DEFAULT NULL,
-   PRIMARY KEY (id)
+   id int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   categoria varchar(100) DEFAULT NULL
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8")
 
 (def categorias-rows
@@ -84,8 +81,8 @@
 ;; Start eventos table
 (def eventos-sql
   "CREATE TABLE eventos (
-   id int(11) unsigned NOT NULL AUTO_INCREMENT,
-   categorias_id int(11) DEFAULT NULL,
+   id int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   categorias_id int(11) unsigned NOT NULL,
    descripcion text DEFAULT NULL,
    fecha_inicio date DEFAULT NULL,
    hora_inicio time DEFAULT NULL,
@@ -94,20 +91,30 @@
    titulo varchar(250) DEFAULT NULL,
    fecha_terminacion date DEFAULT NULL,
    hora_terminacion time DEFAULT NULL,
-   PRIMARY KEY(id)
+   CONSTRAINT `fk_eventos_categorias`
+   FOREIGN KEY (categorias_id) REFERENCES categorias (id)
+   ON DELETE CASCADE
+   ON UPDATE RESTRICT
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8")
 ;; End eventos table
 
 ;; Start registro_evento table
 (def registro_evento-sql
   "CREATE TABLE registro_evento (
-   id int(11) unsigned NOT NULL AUTO_INCREMENT,
-   matricula_id int(11) NOT NULL,
-   eventos_id int(11) NOT NULL,
+   id int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   matricula_id int(11) unsigned NOT NULL,
+   eventos_id int(11) unsigned NOT NULL,
    hora_entrada time DEFAULT NULL,
    hora_salida time DEFAULT NULL,
    fecha date DEFAULT NULL,
-   PRIMARY KEY(id)
+   CONSTRAINT `fk_registro_evento_alumnos`
+   FOREIGN KEY (matricula_id) REFERENCES alumnos (matricula)
+   ON DELETE CASCADE
+   ON UPDATE RESTRICT,
+   CONSTRAINT `fk_registro_evento_eventos`
+   FOREIGN KEY (eventos_id) REFERENCES eventos (id)
+   ON DELETE CASCADE
+   ON UPDATE RESTRICT
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8")
 ;; End registro_evento table
 
@@ -116,25 +123,25 @@
    Note: First create the database on MySQL with any client"
   (Query! db users-sql)
   (Insert-multi db :users user-rows)
-  (Query! db alumnos-sql)
   (Query! db categorias-sql)
   (Insert-multi db :categorias categorias-rows)
+  (Query! db alumnos-sql)
   (Query! db eventos-sql)
   (Query! db registro_evento-sql))
 
 (defn reset-database []
   "Removes existing tables and re-creates them"
+  (Query! db "DROP table IF EXISTS registro_evento")
+  (Query! db "DROP table IF EXISTS eventos")
+  (Query! db "DROP table IF EXISTS alumnos")
+  (Query! db "DROP table IF EXISTS categorias")
   (Query! db "DROP table IF EXISTS users")
   (Query! db users-sql)
   (Insert-multi db :users user-rows)
-  (Query! db "DROP table IF EXISTS alumnos")
-  (Query! db alumnos-sql)
-  (Query! db "DROP table IF EXISTS categorias")
   (Query! db categorias-sql)
   (Insert-multi db :categorias categorias-rows)
-  (Query! db "DROP table IF EXISTS eventos")
+  (Query! db alumnos-sql)
   (Query! db eventos-sql)
-  (Query! db "DROP table IF EXISTS registro_evento")
   (Query! db registro_evento-sql))
 
 (defn migrate []
